e1e9cdf33e53704fbee5878f28b74da617313608
NightStreeetWalk <41827780+NightStreeetWalk@users.noreply.github.com>
2025-07-12 18:59:03 +0800

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://rpc.sepolia.org",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
